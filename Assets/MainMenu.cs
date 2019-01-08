26b83fa02634223ce669f51d6ffeee9dfaf9553d
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2019-01-08 10:26:50 +0100

    Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void ToggleButton(GameObject obj)
+    {
+        obj.GetComponent<Button>().interactable
+        = !obj.GetComponent<Button>().interactable;
+    }
+
+    public void MyTestButton()
+    {
+        print("hej");
+    }
+}
---
+++ Assets/TriangleRotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleRotate : MonoBehaviour
+{
+    public void Rotate(float amount)
+    {
+        transform.Rotate(0, 0, amount);
+    }
+}
